30ae23265893b9d11e071ce1887139b71da5a054
MichaelPAN123 <55261824+MichaelPAN123@users.noreply.github.com>
2019-11-28 14:48:37 -0500

    Add files via upload

---
+++ Script/Sound_one.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_one : MonoBehaviour{
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource> ();
+    }
+	
+	void OnCollisionEnter(Collision collision){  
+
+if (collision.gameObject.name == "Cube") {
+
+  audio.Play ();
+   }
+  }
+ }
